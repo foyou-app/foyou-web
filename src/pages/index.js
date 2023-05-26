6e843bcdaeb02bf18899727a390791f0a3bd125d
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2023-05-26 20:42:05 +0800

    add link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import { useDispatch, useSelector } from 'react-redux';
-import SwiperCore, { Autoplay } from 'swiper';
+import { useDispatch, useSelector } from "react-redux";
+import SwiperCore, { Autoplay } from "swiper";
 // import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
-import Header from './header';
-import Footer from './footer';
-import 'swiper/swiper-bundle.css';
-import { web_data } from '../lib/store/actions/actions';
+import Header from "./header";
+import Footer from "./footer";
+import "swiper/swiper-bundle.css";
+import { web_data } from "../lib/store/actions/actions";
 
 SwiperCore.use([Autoplay]);
 
 const Home = () => {
   const { t } = useTranslation();
-  const lets = useSelector(state => state.mainReducer);
+  const lets = useSelector((state) => state.mainReducer);
   const [data, setdata] = useState(null);
   const [notifications, setnotifications] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(web_data())
-      .then(res => {
+      .then((res) => {
         setdata(res.data);
       })
-      .catch(error => {});
+      .catch((error) => {});
   }, []);
 
   useEffect(() => {
     if (lets.pages != null) {
-      setnotifications(lets.pages.data.filter(x => x.type === 'notif'));
+      setnotifications(lets.pages.data.filter((x) => x.type === "notif"));
     }
   }, [lets]);
 
@@ -47,8 +47,8 @@ const Home = () => {
           <div className="col-xl-5 col-lg-6">
             <div className="row">
               <div className="col-lg-12 col-sm-6 col-12 animate__animated animate__fadeInDown animate__delay-1s">
-                <h2 className="fw-bold fs-2 mt-lg-0 mt-5">{t('Users')}</h2>
-                <p className="fs-5">{t('users_text')}</p>
+                <h2 className="fw-bold fs-2 mt-lg-0 mt-5">{t("Users")}</h2>
+                <p className="fs-5">{t("users_text")}</p>
                 <div className="d-flex align-items-center gap-3 mb-5">
                   <div className="d-flex">
                     <img src="assets/images/demo/1.jpg" alt="" className="ratio-11 w-48 h-48 rounded-5 border-custom" />
@@ -75,15 +75,15 @@ const Home = () => {
                   </div>
                   <div className="d-flex flex-column">
                     <strong className="fs-6">
-                      +{data?.users.count} {t('Users')}
+                      +{data?.users.count} {t("Users")}
                     </strong>
-                    <span className="fs-12">{t('register_please')}</span>
+                    <span className="fs-12">{t("register_please")}</span>
                   </div>
                 </div>
               </div>
               <div className="col-lg-12 col-sm-6 col-12 animate__animated animate__fadeInDown animate__delay-2s">
-                <h2 className="fw-bold fs-2 mt-lg-0 mt-5">{t('customers')}</h2>
-                <p className="fs-5">{t('customers_text')}</p>
+                <h2 className="fw-bold fs-2 mt-lg-0 mt-5">{t("customers")}</h2>
+                <p className="fs-5">{t("customers_text")}</p>
                 <div className="d-flex align-items-center gap-3 mb-5">
                   <div className="d-flex">
                     <img src="assets/images/demo/1.jpg" alt="" className="ratio-11 w-48 h-48 rounded-2 border-custom" />
@@ -110,19 +110,24 @@ const Home = () => {
                   </div>
                   <div className="d-flex flex-column">
                     <strong className="fs-6">
-                      +{data?.pages.count} {t('customers')}
+                      +{data?.pages.count} {t("customers")}
                     </strong>
-                    <span className="fs-12"> {t('publish_survey')}</span>
+                    <span className="fs-12"> {t("publish_survey")}</span>
                   </div>
                 </div>
               </div>
             </div>
             <hr />
             <div className="d-flex flex-column animate__animated animate__fadeInUp animate__delay-2s">
-              <h2 className="fs-6 fw-bold text-lg-start text-center">{t('register_please')}</h2>
-              <h1 className="fs-14 text-lg-start text-center">{t('ads_survey_enter')}</h1>
+              <h2 className="fs-6 fw-bold text-lg-start text-center">{t("register_please")}</h2>
+              <h1 className="fs-14 text-lg-start text-center">{t("ads_survey_enter")}</h1>
               <div className="d-flex flex-wrap justify-content-lg-start justify-content-center gap-2 mb-5">
-                <a href="#" className="d-block">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://play.google.com/store/apps/details?id=com.foyou.app"
+                  className="d-block"
+                >
                   <img src="assets/images/google.svg" alt="" />
                 </a>
                 <a href="#" className="d-block">
@@ -143,7 +148,7 @@ const Home = () => {
             <div className="phone position-relative zindex-1 ms-auto maxw-100 animate__animated animate__fadeInLeft animate__delay-2s">
               <img src="assets/images/phone.png" alt="" className="h-100 img-fluid ratio-21" />
               <div className="swiperAbsolute">
-                {' '}
+                {" "}
                 <div className="swiper  mySwiper">
                   <Swiper
                     spaceBetween={8}
@@ -156,7 +161,7 @@ const Home = () => {
                       reverseDirection: true,
                     }}
                     loop={true}
-                    effect={'flip'}
+                    effect={"flip"}
                     cubeEffect={{
                       shadow: true,
                       slideShadows: true,
@@ -164,10 +169,10 @@ const Home = () => {
                       shadowScale: 0.94,
                     }}
                   >
-                    {notifications.map(num => {
+                    {notifications.map((num) => {
                       return (
                         <SwiperSlide>
-                          {' '}
+                          {" "}
                           <div class="swiper-slide">
                             <div className="d-flex gap-2 p-2 w-100">
                               <img src="assets/images/favicon.svg" alt="" />
@@ -189,7 +194,7 @@ const Home = () => {
                       );
                     })}
                   </Swiper>
-                </div>{' '}
+                </div>{" "}
               </div>
             </div>
           </div>
