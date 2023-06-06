30405f9cb3bd1f47c2bd1c1e632c84b942a7f684
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2023-06-06 17:51:32 +0800

    change text ad to news

--- src/lib/languages/en.js
+++ src/lib/languages/en.js
@@ -5,9 +5,9 @@ module.exports = {
   contact_us: 'Холбоо барих',
   faq: 'About Foyou & F.A.Q',
   register_please: 'Create your account!',
-  ads_survey_enter: 'Publish your ad & survey',
+  ads_survey_enter: 'Publish your news & survey',
   customers: 'Clients',
-  users_text: 'Earn money by reading ad and completing survey on "Foyou" app',
-  customers_text: 'A new space for the most accessible, fastest and cheapest advertisements and surveys. ',
-  publish_survey: 'Publish your ad & survey',
+  users_text: 'Earn money by reading news and completing survey on "Foyou" app',
+  customers_text: 'A new space for the most accessible, fastest and cheapest news and surveys. ',
+  publish_survey: 'Publish your news & survey',
 };
